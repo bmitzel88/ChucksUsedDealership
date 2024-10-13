df52dea7c298c721021dc2c58973e6936b586a88
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-10-13 00:19:47 -0700

    Create the salesPerson class

---
+++ ChucksUsedDealership/Models/SalesPerson.cs
@@ -0,0 +1,30 @@
+﻿namespace ChucksUsedDealership.Models
+{
+    public class SalesPerson
+    {
+        /// <summary>
+        /// The unique identifier for the sales person
+        /// </summary>
+        public int SalesPersonId { get; set; }
+
+        /// <summary>
+        /// First name of the sales person
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name of the sales person
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Email address of the sales person
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Phone number of the sales person
+        /// </summary>
+        public string PhoneNumber { get; set; }
+    }
+}
