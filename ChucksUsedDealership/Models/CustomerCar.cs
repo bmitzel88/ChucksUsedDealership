0b338ee1a3dbd1da49af159f3d94ea4998dee81e
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-10-13 00:53:42 -0700

    Added CustomerCar class for customer profile info storage

---
+++ ChucksUsedDealership/Models/CustomerCar.cs
@@ -0,0 +1,40 @@
+﻿namespace ChucksUsedDealership.Models
+{
+    public class CustomerCar
+    {
+        /// <summary>
+        /// CustomerCarId is the unique identifier for the customer car
+        /// </summary>
+        public int CustomerCarId { get; set; }
+
+        /// <summary>
+        /// CustomerId is the unique identifier for the customer
+        /// </summary>
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// Make is the manufacturer of the car
+        /// </summary>
+        public string Make { get; set; }
+
+        /// <summary>
+        /// Model is the specific model of the car
+        /// </summary>
+        public string Model { get; set; }
+
+        /// <summary>
+        /// Year is the year the car was manufactured
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// VIN is the Vehicle Identification Number of the car
+        /// </summary>
+        public int VIN { get; set; }
+
+        /// <summary>
+        /// Customer is the customer who owns the car
+        /// </summary>
+        public Customer Customer { get; set; }
+    }
+}
