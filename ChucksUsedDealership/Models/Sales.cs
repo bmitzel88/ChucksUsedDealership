17cf3f12bfbd3ae91245673c49824f3035809dca
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-10-13 00:37:07 -0700

    Added the sales class

---
+++ ChucksUsedDealership/Models/Sales.cs
@@ -0,0 +1,35 @@
+﻿namespace ChucksUsedDealership.Models
+{
+    public class Sales
+    {
+        /// <summary>
+        /// The unique identifier for the sales record
+        /// </summary>
+        public int SalesID { get; set; }
+
+        /// <summary>
+        /// The unique identifier for the car being sold
+        /// </summary>
+        public int carID { get; set; }
+
+        /// <summary>
+        /// The unique identifier for the customer buying the car
+        /// </summary>
+        public int CustomerID { get; set; }
+
+        /// <summary>
+        /// The unique identifier for the sales person selling the car
+        /// </summary>
+        public int SalesPersonID { get; set; }
+
+        /// <summary>
+        /// The date of the sale
+        /// </summary>
+        public DateTime SaleDate { get; set; }
+
+        /// <summary>
+        /// The price the car was sold for
+        /// </summary>
+        public decimal SalePrice { get; set; }
+    }
+}
