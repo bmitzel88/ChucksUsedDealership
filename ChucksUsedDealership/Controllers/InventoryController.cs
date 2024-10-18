53c98332459f1971a79e3c43e26aa3c2e404ed95
Brandon <brandonmitzel05@gmail.com>
2024-10-18 15:25:04 -0700

    Added Inventory page and added Admin only feautures in the Inventory controller

---
+++ ChucksUsedDealership/Controllers/InventoryController.cs
@@ -0,0 +1,102 @@
+﻿using ChucksUsedDealership.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace ChucksUsedDealership.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly DealershipDbContext? _context;
+
+        // Inject dealershipDB
+        public InventoryController(DealershipDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: InventoryController
+        public ActionResult Index()
+        {
+            var model = _context.CarInventories.ToList();
+            return View(model);
+        }
+
+        // GET: InventoryController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: InventoryController/Create
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InventoryController/Create
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InventoryController/Edit/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: InventoryController/Edit/5
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InventoryController/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: InventoryController/Delete/5
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
