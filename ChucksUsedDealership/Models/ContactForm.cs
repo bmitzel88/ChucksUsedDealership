a7706715f9774418b2a6fa78dc8de14bbf54ed8d
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-10-20 15:55:19 -0700

    create contact form model

---
+++ ChucksUsedDealership/Models/ContactForm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChucksUsedDealership.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+
+}
