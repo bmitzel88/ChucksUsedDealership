14b116e96cc61473b18ee75893c2d9c4b28526ec
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-10-26 15:32:00 -0700

    migrated and updated contact form model

--- ChucksUsedDealership/Data/Migrations/20241026222700_Added Phone and date field in contact form model.Designer.cs
+++ ChucksUsedDealership/Data/Migrations/20241026222700_Added Phone and date field in contact form model.Designer.cs
@@ -4,6 +4,7 @@ using ChucksUsedDealership.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChucksUsedDealership.Data.Migrations
 {
     [DbContext(typeof(DealershipDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241026222700_Added Phone and date field in contact form model")]
+    partial class AddedPhoneanddatefieldincontactformmodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
