bb41e7368d128e037d28a5cf6e0afeb1c1ff9eb7
Brandon <brandonmitzel05@gmail.com>
2024-10-27 15:03:14 -0700

    Created InventoryViewModel

---
+++ ChucksUsedDealership/Models/InventoryViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ChucksUsedDealership.Models
+{
+    public class InventoryViewModel
+    {
+        public InventoryViewModel(List<CarInventory> cars, int lastPage, int currPage) 
+        {
+            Cars = cars;
+            LastPage = lastPage;
+            CurrentPage = currPage;
+        }
+
+        /// <summary>
+        /// The full list of cars in stock
+        /// </summary>
+        public List<CarInventory> Cars { get; private set; }
+        
+        /// <summary>
+        /// The last page of the Inventory of cars
+        /// </summary>
+        public int LastPage { get; private set; }
+
+        /// <summary>
+        /// The current page the user is viewing
+        /// </summary>
+        public int CurrentPage { get; private set; }
+    }
+}
