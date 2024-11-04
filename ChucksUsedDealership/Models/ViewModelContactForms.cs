b7e6ff02243dee12bc9db61246d5198476034212
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-11-03 17:33:16 -0800

    Create class for pagination

---
+++ ChucksUsedDealership/Models/ViewModelContactForms.cs
@@ -0,0 +1,25 @@
+﻿namespace ChucksUsedDealership.Models
+{
+    public class ViewModelContactForms
+    {
+        /// <summary>
+        /// List of cars to display on the page
+        /// </summary>
+        public List<ContactForm> ContactForms { get; set; }
+
+        /// <summary>
+        /// Stores the current page number
+        /// </summary>
+        public int CurrentPage { get; set; }
+
+        /// <summary>
+        /// Stores the total number of pages
+        /// </summary>
+        public int TotalPages { get; set; }
+
+        /// <summary>
+        /// Stores the number of cars to display per page
+        /// </summary>
+        public int PageSize { get; set; }
+    }
+}
